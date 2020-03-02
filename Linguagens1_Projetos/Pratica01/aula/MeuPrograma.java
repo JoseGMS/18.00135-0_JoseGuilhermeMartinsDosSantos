cba3a5222723c7fffb0fd20ff43aa3b063ae55a8
JoseGMS <josegms2000@gmail.com>
2020-03-02 11:26:03 -0300

    Meu arquivo java do primeiro programa

---
+++ Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
@@ -0,0 +1,8 @@
+class MeuPrograma {
+    public static void main(String[] args) {
+
+        //Mensagem de saida do sistema 
+        System.out.println("Olá Mundo");
+        System.out.println("Estou programando no modo Conan. :)");
+    }
+}
