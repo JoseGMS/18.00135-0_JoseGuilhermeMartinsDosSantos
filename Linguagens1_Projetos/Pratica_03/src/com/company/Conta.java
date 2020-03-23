c0e7e7c30346d78396cc309a7205c9269c56281f
JoseGMS <josegms2000@gmail.com>
2020-03-23 12:24:06 -0300

    aula 03

---
+++ Linguagens1_Projetos/Pratica_03/src/com/company/Conta.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Conta {
+    private double saldo;
+    public void sacar(double valor){
+        if(saldo >= valor)
+            saldo -= valor; //saldo = saldo - valor;
+    }
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+    public double consultaSaldo(){
+        return this.saldo;
+    }
+    public Conta(int saldoInicial){
+        this.saldo = saldoInicial;
+
+    }
+}
